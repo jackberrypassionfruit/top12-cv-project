b8d22830a60a55b3737a808b52aa7fdf2f5605d9
jackberrypassionfruit <pashayanjack@gmail.com>
2022-08-17 22:40:52 -0400

    Added components and brainstorming.txt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Preview from './components/Preview';
+import { jsPDF } from 'jspdf';
+// https://github.com/parallax/jsPDF
+
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <p>CV Builder</p>
+      </header>
+
+      <div className="editor"></div>
+      <Preview/>
     </div>
   );
 }
---
+++ src/components/Preview.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Preview extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <p>I'm back bitches!</p>
+    )
+  }
+}
+
+export default Preview
